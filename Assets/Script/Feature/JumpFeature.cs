fd6d2c43238200e595d48f176d8e21cc5e7fefa7
pkc <kaosumog@gmail.com>
2025-02-06 10:46:06 +0900

    [UPDATED] : player movement code

--- Assets/Script/Feature/JumpFeature.cs
+++ Assets/Script/Feature/JumpFeature.cs
@@ -7,25 +7,24 @@ namespace Game
     [RequireComponent(typeof(BoxCollider2D))]
     public class JumpFeature : NetFeature
     {
-        [SerializeField] private float maxJumpForce = 12f;
-        [SerializeField] private float minJumpForce = 3f;
-        [SerializeField] private float jumpChargeTime = 0.2f;
-        [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private float groundCheckDistance = 0.1f;
+        [SerializeField] private float MaxJumpForce = 12f;
+        [SerializeField] private float MinJumpForce = 3f;
+        [SerializeField] private float JumpChargeTime = 0.2f;
+        [SerializeField] private LayerMask GroundLayer;
+        [SerializeField] private float GroundCheckDistance = 0.1f;
 
-        private Rigidbody2D body;
-        private BoxCollider2D boxCollider;
-        private bool isJumping;
-        private float jumpStartTime;
-        private bool isGrounded;
-        private float currentJumpForce;
-        private Vector2 groundCheckSize;
+        private Rigidbody2D _body;
+        private BoxCollider2D _boxCollider;
+        private bool _isJumping;
+        private float _jumpStartTime;
+        private bool _isGrounded;
+        private Vector2 _groundCheckSize;
 
         void Awake()
         {
-            body = GetComponent<Rigidbody2D>();
-            boxCollider = GetComponent<BoxCollider2D>();
-            groundCheckSize = new Vector2(boxCollider.bounds.size.x * 0.9f, 0.1f);
+            _body = GetComponent<Rigidbody2D>();
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _groundCheckSize = new Vector2(_boxCollider.bounds.size.x * 0.9f, 0.1f);
         }
 
         protected override void OwnerUpdate()
@@ -36,39 +35,33 @@ namespace Game
 
         private void CheckGrounded()
         {
-            Vector2 boxCenter = new Vector2(transform.position.x, boxCollider.bounds.min.y);
-            RaycastHit2D hit = Physics2D.BoxCast(boxCenter, groundCheckSize, 0f, Vector2.down, groundCheckDistance, groundLayer);
-            isGrounded = hit.collider != null;
-
-            //Debug
-            //Color debugColor = isGrounded ? Color.green : Color.red;
-            //Debug.DrawRay(boxCenter, Vector2.down * groundCheckDistance, debugColor);
-            //Debug.DrawLine(boxCenter - new Vector2(groundCheckSize.x / 2, 0), boxCenter + new Vector2(groundCheckSize.x / 2, 0), debugColor);
+            Vector2 boxCenter = new Vector2(transform.position.x, _boxCollider.bounds.min.y);
+            RaycastHit2D hit = Physics2D.BoxCast(boxCenter, _groundCheckSize, 0f, Vector2.down, GroundCheckDistance, GroundLayer);
+            _isGrounded = hit.collider != null;
         }
 
         private void HandleJumpInput()
         {
-            if (Input.GetButtonDown("Jump") && isGrounded)
+            if (Input.GetButtonDown("Jump") && _isGrounded)
             {
-                isJumping = true;
-                jumpStartTime = Time.time;
-                currentJumpForce = minJumpForce;
-                body.velocity = new Vector2(body.velocity.x, currentJumpForce);
+                _isJumping = true;
+                _jumpStartTime = Time.time;
+                _body.velocity = new Vector2(_body.velocity.x, MinJumpForce);
             }
 
-            if (Input.GetButton("Jump") && isJumping)
+            if (Input.GetButton("Jump") && _isJumping)
             {
-                float holdTime = Time.time - jumpStartTime;
-                if (holdTime < jumpChargeTime)
+                float holdTime = Time.time - _jumpStartTime;
+                if (holdTime < JumpChargeTime)
                 {
-                    float extraForce = Mathf.Lerp(0f, maxJumpForce - minJumpForce, holdTime / jumpChargeTime);
-                    body.velocity = new Vector2(body.velocity.x, minJumpForce + extraForce);
+                    float extraForce = Mathf.Lerp(0f, MaxJumpForce - MinJumpForce, holdTime / JumpChargeTime);
+                    _body.velocity = new Vector2(_body.velocity.x, MinJumpForce + extraForce);
                 }
             }
 
-            if (Input.GetButtonUp("Jump") || (Time.time - jumpStartTime >= jumpChargeTime) || body.velocity.y < 0)
+            if (Input.GetButtonUp("Jump") || (Time.time - _jumpStartTime >= JumpChargeTime) || _body.velocity.y < 0)
             {
-                isJumping = false;
+                _isJumping = false;
             }
         }
     }
--- Assets/Script/Feature/RunFeature.cs
+++ Assets/Script/Feature/RunFeature.cs
@@ -4,79 +4,70 @@ using UnityEngine;
 namespace Game
 {
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class RunFeature : NetFeature
     {
         private const float STOP_THRESHOLD = 0.1f;
 
-        [SerializeField] private float maxSpeed = 5f;
-        [SerializeField] private float acceleration = 10f;
-        [SerializeField] private float deceleration = 5f;
-        [SerializeField] private float friction = 2f;
-        [SerializeField] private LayerMask wallLayer;
-        [SerializeField] private float wallCheckDistance = 0.1f;
+        [SerializeField] private float MaxSpeed = 5f;
+        [SerializeField] private float Acceleration = 10f;
+        [SerializeField] private float Deceleration = 5f;
+        [SerializeField] private float Friction = 2f;
+        [SerializeField] private LayerMask WallLayer;
+        [SerializeField] private float WallCheckDistance = 0.1f;
 
-        private Rigidbody2D body;
-        private float moveInput;
-        private bool isTouchingWall;
+        private Rigidbody2D _body;
+        private BoxCollider2D _collider;
+        private float _moveInput;
 
         void Awake()
         {
-            body = GetComponent<Rigidbody2D>();
+            _body = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<BoxCollider2D>();
         }
 
         protected override void OwnerUpdate()
         {
-            moveInput = Input.GetAxis("Horizontal");
-            CheckWallCollision();
+            _moveInput = Input.GetAxis("Horizontal");
         }
 
         protected override void OwnerFixedUpdate()
         {
-            if (!isTouchingWall)
-            {
-                ApplyMovement();
-            }
-            else
-            {
-                body.velocity = new Vector2(0, body.velocity.y);
-            }
+            ApplyMovement();
         }
 
         private void ApplyMovement()
         {
-            if (moveInput != 0)
+            if (!Mathf.Approximately( _moveInput,0))
             {
-                float targetSpeed = moveInput * maxSpeed;
-                float speedDiff = targetSpeed - body.velocity.x;
-                float accelerationRate = Mathf.Abs(targetSpeed) > STOP_THRESHOLD ? acceleration : deceleration;
-                float movement = accelerationRate * speedDiff * Time.fixedDeltaTime;
-                body.velocity += new Vector2(movement, 0);
+                //Wall AntiStick
+                if ((_moveInput > 0 && !CheckDirection(Vector2.right)) || (_moveInput < 0 && !CheckDirection(Vector2.left)))
+                {
+                    float targetSpeed = _moveInput * MaxSpeed;
+                    float speedDiff = targetSpeed - _body.velocity.x;
+                    float accelerationRate = Mathf.Abs(targetSpeed) > STOP_THRESHOLD ? Acceleration : Deceleration;
+                    float movement = accelerationRate * speedDiff * Time.fixedDeltaTime;
+                    _body.velocity += new Vector2(movement, 0);
+                    return;
+                }
+            }
+            
+            if (Mathf.Abs(_body.velocity.x) > STOP_THRESHOLD)
+            {
+                float slowDown = Mathf.Sign(_body.velocity.x) * Friction * Time.fixedDeltaTime;
+                _body.velocity -= new Vector2(slowDown, 0);
             }
             else
             {
-                if (Mathf.Abs(body.velocity.x) > STOP_THRESHOLD)
-                {
-                    float slowDown = Mathf.Sign(body.velocity.x) * friction * Time.fixedDeltaTime;
-                    body.velocity -= new Vector2(slowDown, 0);
-                }
-                else
-                {
-                    body.velocity = new Vector2(0, body.velocity.y);
-                }
+                _body.velocity = new Vector2(0, _body.velocity.y);
             }
         }
 
-        private void CheckWallCollision()
+        bool CheckDirection(Vector2 dir)
         {
-            Vector2 boxSize = new Vector2(0.1f, body.GetComponent<Collider2D>().bounds.size.y * 0.9f);
+            Vector2 boxSize = new Vector2(0.1f, _collider.bounds.size.y * 0.9f);
             Vector2 boxCenter = (Vector2)transform.position;
-            isTouchingWall = Physics2D.OverlapBox(boxCenter + Vector2.left * wallCheckDistance, boxSize, 0, wallLayer) ||
-                             Physics2D.OverlapBox(boxCenter + Vector2.right * wallCheckDistance, boxSize, 0, wallLayer);
-
-            //Debug
-            //Color debugColor = isTouchingWall ? Color.red : Color.green;
-            //Debug.DrawRay(boxCenter, Vector2.left * wallCheckDistance, debugColor);
-            //Debug.DrawRay(boxCenter, Vector2.right * wallCheckDistance, debugColor);
+            return Physics2D.OverlapBox(boxCenter + dir * WallCheckDistance, boxSize, 0, WallLayer) != null;
         }
     }
 }
